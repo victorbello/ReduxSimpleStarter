dd82a5c68df2d10feab5e5b59df7e9ef4a672aac
Victor Bello <Victor.Bello@ff.com>
2017-07-06 11:12:11 -0700

    Search Bar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAgWVTXHJsFHufidJKRgdA91iXVOrkumVo';
 
 const App = () => {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
