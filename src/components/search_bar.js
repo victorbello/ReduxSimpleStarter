589b0f902b902d085459fb8edd8e19bf14b291fc
Victor Bello <Victor.Bello@ff.com>
2017-07-06 11:20:32 -0700

    Class based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return (
-    <input />
-  );
-};
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <input />
+    );
+  }
+}
 
 export default SearchBar;
