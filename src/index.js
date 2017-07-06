6fbffc50b746ca3d0f5c831073683005eaab3319
Victor Bello <Victor.Bello@ff.com>
2017-07-06 12:22:00 -0700

    Props

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAgWVTXHJsFHufidJKRgdA91iXVOrkumVo';
 
@@ -22,6 +23,7 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
