ddd4cc9b66090d0dde264cd93cb66911ed0fbf62
Victor Bello <Victor.Bello@ff.com>
2017-07-06 12:14:48 -0700

    Refactoring functional components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAgWVTXHJsFHufidJKRgdA91iXVOrkumVo';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
