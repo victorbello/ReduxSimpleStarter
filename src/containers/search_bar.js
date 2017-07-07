e46fd98655a9553febcbe470d7ad7911431b0b90
Victor Bello <Victor.Bello@ff.com>
2017-07-07 12:34:34 -0700

    Binding context

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = {
+    term: ''
+  };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
   render() {
     return (
       <form className="input-group">
-        <input placeholder="Search for a city..." />
+        <input
+          placeholder="Get a five day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">
             Submit
