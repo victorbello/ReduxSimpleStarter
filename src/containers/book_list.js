357865ee20d6099980c5cc25b93986706e344dd0
Victor Bello <Victor.Bello@ff.com>
2017-07-07 10:31:52 -0700

    Creating an action

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends React.Component {
   renderList = () => {
     return this.props.books.map(book => {
       return (
-        <li key={book.title} className="list-group-item">
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item"
+        >
           {book.title}
         </li>
       );
@@ -25,6 +31,10 @@ const mapStateToProps = state => {
   return {
     books: state.books
   };
-}
+};
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ selectBook }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
