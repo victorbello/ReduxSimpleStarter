97165f01ef987cf2cfb8dee80f2133c202536a58
Victor Bello <Victor.Bello@ff.com>
2017-07-10 13:23:36 -0700

    No Mutations

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,10 @@ import { FETCH_WEATHER } from '../actions/index';
 export default (state = [], action) => {
   switch (action.type) {
     case FETCH_WEATHER:
-      return state.concat([action.payload.data]);
+      return [
+        action.payload.data,
+        ...state
+      ];
       break;
     default:
       return state;
