a6621a94363ba9066b2c17941440aa3083db7109
Victor Bello <Victor.Bello@ff.com>
2017-07-13 16:37:00 -0700

    Form submittal

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -9,26 +9,61 @@ class PostsNew extends React.Component {
           {field.label}
         </label>
         <input className="form-control" type="text" {...field.input} />
+        {field.meta.error}
       </div>
     );
   };
 
-  renderTagsField = field => {};
+  onSubmit = values => {
+    console.log(values);
+  }
 
   render() {
+    const { handleSubmit } = this.props;
+
     return (
       <div>
         <h3>New Post</h3>
-        <form>
-          <Field label="Title" name="title" component={this.renderField} />
-          <Field label="Tags" name="tags" component={this.renderField} />
-          <Field label="Post Content" name="content" component={this.renderField} />
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <Field
+            label="Title for the Post"
+            name="title"
+            component={this.renderField}
+          />
+          <Field
+            label="Categories"
+            name="categories"
+            component={this.renderField}
+          />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField}
+          />
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </form>
       </div>
     );
   }
 }
 
+const validate = values => {
+  const errors = {};
+  if (!values.title) {
+    errors.title = 'Enter a title!';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter a category!';
+  }
+  if (!values.content) {
+    errors.content = 'Enter content!';
+  }
+  return errors;
+};
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
