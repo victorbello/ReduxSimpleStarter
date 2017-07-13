3560b0e539c0ca9ee123acf6628a1d25e0cfc98f
Victor Bello <Victor.Bello@ff.com>
2017-07-13 12:34:09 -0700

    The field component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends React.Component {
+  renderTitleField = field => {
+    return (
+      <div>
+        <input
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  };
+
   render() {
     return (
       <div>
-        Posts New!
+        <h3>New Post</h3>
+        <form>
+          <Field name="title" component={this.renderTitleField} />
+        </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
