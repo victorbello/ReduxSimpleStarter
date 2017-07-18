0ada2fbc6ba108b59f00c7a7914c796c9a9a2306
Victor Bello <Victor.Bello@ff.com>
2017-07-18 14:59:58 -0700

    Posts show component

---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PostsShow extends React.Component {
+  render() {
+    return (
+      <div>
+        Posts Show!
+      </div>
+    );
+  }
+}
+
+export default PostsShow;
